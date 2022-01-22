f155420891dd8feed2462d7fad62a54d0a5b6680
hzz4343 <hzz1996@qq.com>
2022-01-22 17:58:20 +0800

    C-Learning1.22

--- coding.cc
+++ coding.cc
@@ -502,27 +502,187 @@
 //     return 0;
 // }
 
-int main()
+// int main()
+// {
+//     int i = 0;
+//     int a = 1;
+//     int count = 0;
+//     for(i=100;i<=200;i++)
+//     {
+//         for(a=2;a<i;a++)
+//         {
+//             if(i%a==0)
+//             {
+//                 break;
+//             }
+//         }
+//         if (i==a)
+//         {
+//             printf("%d\n",i);
+//         }
+        
+//         }
+    
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     char arr1[20]={0};
+//     char arr2[]="hello bit";
+//     strcpy(arr1,arr2);
+//     printf("%s\n",arr1);
+//     return 0;
+// }
+
+// int main()
+// {
+//     char arr1[]="hello bit";
+//     memset(arr1,'z',5);
+//     printf("%s\n",arr1);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 0;
+//     int b = 2;
+//     a = b ;
+//     printf("%d %d",a,b);
+
+//     return 0;
+// }
+
+// int get_max(int x, int y)
+// {
+//     int z = 0;
+//     if(x>y)
+//     {
+//         z = x;
+//     }
+//     else
+//     {
+//         z = y;
+//     }
+//     return z;
+
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     int max = get_max(a,b);
+//     printf("max = %d\n",max);
+
+
+
+//     return 0;
+// }
+
+// void exchange(int *pa, int *pb)
+// {
+//     int z = 0;
+//     z = *pa;
+//     *pa = *pb;
+//     *pb = z;
+// }
+
+// int main()
+// {
+//     int a = 5;
+//     int b = 6;
+//     exchange(&a,&b);
+//     printf("a=%d,b=%d\n",a,b);
+
+//     return 0;
+// }
+
+
+// int is_prime(int x)
+// {
+//     int y = 2;
+//     for(y=2;y<x;y++)
+//     {if (x%y==0)
+//     {return 0;}
+//     }
+//     return 1;
+// }
+
+// int main()
+// {
+//     int i = 0;
+//     for(i=100;i<=200;i++)
+//     {
+//         if(is_prime(i)==1)//判断是不是素数
+//         {
+//             printf("%d\n",i);
+//         }
+
+//     }
+
+//     return 0;
+// }
+
+// int is_leap_year(int x)
+// {
+//     if (x%400==0 || ((x%100!=0) && (x % 4 ==0)))
+//     return 1;
+//     else
+//     return 0;
+// }
+
+// int main()
+// {
+//     int y = 0;
+//     for (y=1000;y<=2000;y++)
+//     {
+//         if (is_leap_year(y)==1)
+//         {
+//             printf("%d\n",y);
+//         }
+//     }
+
+
+
+
+//     return 0;
+// }
+
+int binary_search(int x[],int y, int z)
 {
     int i = 0;
-    int a = 1;
-    int count = 0;
-    for(i=100;i<=200;i++)
+    int left = 0;
+    int right = z-1;
+    while(left<=right)
+    {    int mid = (left+right)/2;
+        if(x[mid]>y)
+        {
+            right = mid-1;
+        }
+        else if (x[mid]<y) 
+        {   left = mid+1;}
+        else if (x[mid]==y)
+            return mid;
+}
+    return -1;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int key = 14;
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    int ret = binary_search(arr,key,sz);
+
+    if (-1 == ret)
     {
-        for(a=2;a<i;a++)
-        {
-            if(i%a==0)
-            {
-                break;
-            }
-        }
-        if (i==a)
-        {
-            printf("%d\n",i);
-        }
-        
-        }
-    
+        printf("找不到\n");
+    }
+    else
+        printf("下标是:%d\n",ret);
 
     return 0;
 }
